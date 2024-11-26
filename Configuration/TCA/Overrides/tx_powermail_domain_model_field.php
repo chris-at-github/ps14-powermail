d2f3f4bcb164cedb1a7342e064759ac41bf091ab
Christian Pschorr <pschorr.christian@gmail.com>
2024-11-26 20:43:41 +0100

    Hinzufügen von Feldbreiten (als neues Feld) im T3 Backend

--- Configuration/TCA/Overrides/tx_powermail_domain_model_field.php
+++ Configuration/TCA/Overrides/tx_powermail_domain_model_field.php
@@ -1,1 +1,27 @@
-<?php
+<?php defined('TYPO3') or die();
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Weitere Felder in tx_powermail_domain_model_field
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_powermail_domain_model_field', [
+	'tx_powermail_column_width' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:ps14_powermail/Resources/Private/Language/locallang_tca.xlf:field.column-width',
+		'config' => [
+			'type' => 'select',
+			'renderType' => 'selectSingle',
+			'items' => [
+				['', 0],
+			],
+			'size' => 1,
+			'maxitems' => 1,
+			'eval' => ''
+		],
+	],
+]);
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Felder den Paletten hinzufuegen
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('tx_powermail_domain_model_field', '4', 'tx_powermail_column_width', 'after:css');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('tx_powermail_domain_model_field', '41', 'tx_powermail_column_width', 'after:css');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('tx_powermail_domain_model_field', '42', 'tx_powermail_column_width', 'after:css');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('tx_powermail_domain_model_field', '43', 'tx_powermail_column_width', 'after:css');
---
+++ ext_tables.sql
@@ -0,0 +1,6 @@
+#
+# Extend Pages
+#
+CREATE TABLE tx_powermail_domain_model_field (
+	tx_powermail_column_width varchar(50) DEFAULT '0' NOT NULL
+);
